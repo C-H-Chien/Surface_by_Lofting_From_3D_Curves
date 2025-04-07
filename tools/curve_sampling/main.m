02c2d4b35269dd3557a1fc60ccf195daf812253a
C-H-Chien <chiangheng.chien@gmail.com>
2025-04-06 23:13:41 -0400

    Scale GT curve points if using ABC-NEF dataset only

--- tools/curve_sampling/main.m
+++ tools/curve_sampling/main.m
@@ -42,48 +42,54 @@ end
 
 %>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 %> Scale down the ground-truth curves to fit it within a 1x1x1 bounding box
+%> This is only applied when using the ABC-NEF dataset
 %>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-scale_min = [inf inf inf];
-scale_max = [-inf -inf -inf];
-
-for i = 1:length(curve_points)
-    c = curve_points{i};
-    c_min = min(c);
-    c_max = max(c);
-    scale_min = min([scale_min; c_min]);
-    scale_max = max([scale_max; c_max]);
+if strcmp(dataset_name, "ABC-NEF/")
+    scale_min = [inf inf inf];
+    scale_max = [-inf -inf -inf];
+    
+    for i = 1:length(curve_points)
+        c = curve_points{i};
+        c_min = min(c);
+        c_max = max(c);
+        scale_min = min([scale_min; c_min]);
+        scale_max = max([scale_max; c_max]);
+    end
+    %> calculate the scale factor. Using this factor to contain the object in a
+    %1 by 1 by 1 bounding box
+    factor = max([scale_max - scale_min]');
+    factor = 1/factor;
+    
+    %> Move all curve points to the first quatriple and scale up/down so that 
+    %  one of the axis is in the interval [0,1]
+    point_location_max = [-inf -inf -inf];
+    shift_curves = curve_points;
+    for i = 1:length(curve_points)
+        c = shift_curves{i};
+        c(:,1) = (c(:,1) - scale_min(1))*factor;
+        c(:,2) = (c(:,2) - scale_min(2))*factor;
+        c(:,3) = (c(:,3) - scale_min(3))*factor;
+        shift_curves{i} = c;
+    
+        c_max = max(c);
+        point_location_max = max([point_location_max; c_max]);
+    end
+    
+    %> Shift the entire curve points to center at (0.5, 0.5)
+    figure;
+    final_curves = shift_curves;
+    for i = 1:size(final_curves, 2)
+        displacement = 0.5 - (point_location_max ./ 2);
+        c = final_curves{i};
+        c(:,1) = c(:,1) + displacement(1);
+        c(:,2) = c(:,2) + displacement(2);
+        c(:,3) = c(:,3) + displacement(3);
+        final_curves{i} = c;
+    end
+else
+    final_curves = curve_points;
 end
-%> calculate the scale factor. Using this factor to contain the object in a
-%1 by 1 by 1 bounding box
-factor = max([scale_max - scale_min]');
-factor = 1/factor;
 
-%> Move all curve points to the first quatriple and scale up/down so that 
-%  one of the axis is in the interval [0,1]
-point_location_max = [-inf -inf -inf];
-shift_curves = curve_points;
-for i = 1:length(curve_points)
-    c = shift_curves{i};
-    c(:,1) = (c(:,1) - scale_min(1))*factor;
-    c(:,2) = (c(:,2) - scale_min(2))*factor;
-    c(:,3) = (c(:,3) - scale_min(3))*factor;
-    shift_curves{i} = c;
-
-    c_max = max(c);
-    point_location_max = max([point_location_max; c_max]);
-end
-
-%> Shift the entire curve points to center at (0.5, 0.5)
-figure;
-final_curves = shift_curves;
-for i = 1:size(final_curves, 2)
-    displacement = 0.5 - (point_location_max ./ 2);
-    c = final_curves{i};
-    c(:,1) = c(:,1) + displacement(1);
-    c(:,2) = c(:,2) + displacement(2);
-    c(:,3) = c(:,3) + displacement(3);
-    final_curves{i} = c;
-end
 %>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 for i = 1:size(final_curves, 2)
