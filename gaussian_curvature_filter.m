3fc47951fd5323a1756a4788b7ae718a67604040
Sanithu Heengama <sanithu_heengama@brown.edu>
2026-04-27 11:46:22 -0400

    Reorganize surface reconstruction code

--- gaussian_curvature_filter.m
+++ gaussian_curvature_filter.m
@@ -10,27 +10,32 @@ pairs = load(fullfile(pwd, 'tmp', 'curves_proximity_pairs.mat')).curves_proximit
 PARAMS.TAU_GAUSSIAN                        = 0.35;
 PARAMS.PLOT                                = 0;
 PARAMS.HAS_GROUND_TRUTH                    = 1;
-pairs_after_curvature_filter = [];
-res = [];
-parfor i = 1:size(pairs, 1)
+
+nPairs = size(pairs, 1);
+pairs_after_curvature_filter = -ones(nPairs, 5);
+res = nan(nPairs, 4);
+
+parfor i = 1:nPairs
     n1 = pairs(i, 1);
     n2 = pairs(i, 2);
     fname1 = fullfile(pwd, 'blender', 'output', "loftsurf_" + int2str(n1) + "_" + int2str(n2) + "_normal.ply");
     fname2 = fullfile(pwd, 'blender', 'output', "loftsurf_" + int2str(n1) + "_" + int2str(n2) + "_reverse.ply");
-    s1 = nan; s2 = nan;
     gc1 = nan; gc2 = nan;
 
     %> calculate curvature of two surface posibility from 1 pair of curves
     try
         [tri,pts] = plyread(fname1,'tri');
-
         gc1 = mean(curvatures(pts(:, 1), pts(:, 2), pts(:, 3), double(tri)));
+    catch ME
+        warning('Failed curvature read for %s: %s', fname1, ME.message);
     end
     try
         [tri,pts] = plyread(fname2,'tri');
         gc2 = mean(curvatures(pts(:, 1), pts(:, 2), pts(:, 3), double(tri)));
+    catch ME
+        warning('Failed curvature read for %s: %s', fname2, ME.message);
     end
-    res = [res; [n1 n2 gc1 gc2]];
+    res(i, :) = [n1 n2 gc1 gc2];
     
     %> Keep the surface if there is 1 valid posibility
     if isnan(gc1) && (~isnan(gc2)) && abs(gc2) < PARAMS.TAU_GAUSSIAN
@@ -47,23 +52,21 @@ parfor i = 1:size(pairs, 1)
     %> with lower curvature
     if abs(gc1) < PARAMS.TAU_GAUSSIAN && abs(gc1) < abs(gc2)
         pairs_after_curvature_filter(i, :) = [n1 n2 1 gc1 gc2];
-        % copyfile(fname1, "./blender/gaussian/");
         continue;
     end
 
     if abs(gc2) < PARAMS.TAU_GAUSSIAN && abs(gc2) < abs(gc1)
         pairs_after_curvature_filter(i, :) = [n1 n2 0 gc1 gc2];
-        % copyfile(fname2, "./blender/gaussian/");
         continue;
     end
 
     if abs(gc1) < PARAMS.TAU_GAUSSIAN || abs(gc2) < PARAMS.TAU_GAUSSIAN
         pairs_after_curvature_filter(i, :) = [n1 n2 1 gc1 gc2];
-        % copyfile(fname1, "./blender/gaussian/");
+    else
+        pairs_after_curvature_filter(i, :) = [-1 -1 -1 -1 -1];
     end
-
-    pairs_after_curvature_filter(i, :) = [-1 -1 -1 -1 -1];
 end
+
 %> save
 pairs_after_curvature_filter(pairs_after_curvature_filter(:, 1) == -1, :) = [];
 save(fullfile(pwd, 'tmp', 'pairs_after_curvature_filter'), "pairs_after_curvature_filter");
@@ -75,15 +78,10 @@ if PARAMS.PLOT
     hold on;
     if PARAMS.HAS_GROUND_TRUTH
         manual_pick = load(fullfile(pwd, 'data', 'manual_pick.mat')).manual_pick;
-        v = [];
-        for i = 1:size(res, 1)
-            for j = 1:size(manual_pick, 1)
-                if res(i, 1) == manual_pick(j, 1) && res(i, 2) == manual_pick(j, 2)
-                    v = [v; res(i, :)];
-                end
-            end
+        hitMask = ismember(res(:, 1:2), manual_pick(:, 1:2), 'rows');
+        if any(hitMask)
+            histogram(min(abs(res(hitMask, 3:4)), [], 2), "EdgeColor","red", "NumBins",10);
         end
     end
-    histogram(min(abs(v(:, 3:4)), [], 2), "EdgeColor","red", "NumBins",10);
     hold off;
 end
